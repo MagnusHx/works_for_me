88ed196f3e0715b700329b851a478610ca3af75f
MagnusHx <skole@mhl5550.localdomain>
2026-01-09 11:07:02 +0100

    cookiecutter

---
+++ works_for_me/src/audio_emotion/train.py
@@ -0,0 +1,10 @@
+from audio_emotion.model import Model
+from audio_emotion.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
