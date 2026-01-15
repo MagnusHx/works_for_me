8077c9db5ed99be779dab69444ed9afdbbaaaba0
KaergaardThomas <kaergaardthomas@gmail.com>
2026-01-15 14:10:58 +0100

    I have coded the model (VGG16 style)

--- src/audio_emotion/train.py
+++ src/audio_emotion/train.py
@@ -2,6 +2,7 @@ import hydra
 from omegaconf import DictConfig
 from audio_emotion.model import Model
 from audio_emotion.data import AudioDataset
+import torch
 
 @hydra.main(
     version_base=None,
@@ -13,8 +14,16 @@ def train(cfg: DictConfig):
     dataset = AudioDataset("data/raw")
     print(len(dataset))
     dataset.preprocess("data/processed")
+
+    # ---- Model fra Hydra cfg ----
+    model = Model(cfg)
+
+    # Dette blev brugt som et sikkerhedstjekt. Det virkede første hug (:
+    # x = torch.randn(2, cfg.model.in_channels, 128, 256)  # dummy spectrogram batch
+    # y = model(x)
+    # print("Output shape:", y.shape)  # forvent (2, cfg.model.num_classes)
     
-    
+
     #waveform, label = dataset[0]
     #model = Model()
     # add rest of your training code here
